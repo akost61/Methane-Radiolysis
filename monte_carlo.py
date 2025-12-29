af20773517eb9352c56afaf51a874f13c66cfcfc
kostiuka <alex8kostiuk@gmail.com>
2025-12-29 15:09:22 -0500

    Updates to ion_event and run_sim
    ion_event corrected for the energy to be divided amongst daughter and primary electron to be E- delta_k
    
    run_sim optimized for Numba and batch data organized later.

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,71 +1,36 @@
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from cross_section import event_names, delta_k
-from run_simulation import run_simulations
+from run_simulation import get_batch_data
 import numpy as np
 
-def create_pdf(simulation, N_sim, initial_ev):
-    print(simulation[0])
-    num_simulations = np.arange(1, simulation.shape[0] + 1) * 1000
-    x_values = np.arange(1, N_sim + 1, 1000)
-    mean_data = np.zeros((len(simulation),28), np.float64)
-    print(num_simulations)
-    for i in range(len(simulation)):
-        mean_data[i] = (simulation[i]/num_simulations[i])
-    
-
-    if len(x_values) < len(simulation):
-        x_values = np.append(x_values, N_sim)
-
-    with PdfPages(f"/Users/emmajia/Desktop/MachineCode Radiolysis/MonteCarloSimulation/{int(initial_ev/1000)}keV_mean_sim.pdf") as pdf:
+def create_pdf(batch_data, initial_eV, total_simulations):
+    x_values = np.arange(1000, total_simulations + 1, 1000)
+    mean_data = batch_data[-1]
+    with PdfPages(f"/Users/emmajia/Documents/GitHub/Methane-Radiolysis/MonteCarloSimulation/{int(initial_eV/1000)}keV_mean_sim.pdf") as pdf:
 
         for i in range(len(event_names)):
-            mean_value = mean_data[-1][i]
+            mean_value = mean_data[i]
             plus_2 = mean_value * 1.02
             minus_2 = mean_value * 0.98
             
             plt.figure(figsize=(8, 6))
-            plt.title(f'Initial Electron at {int(initial_ev/1000)}keV For {event_names[i]} Mean Occurance vs Simulations Ran')
-            plt.xlabel('Simulation runs')
-            plt.ylabel('Mean occurance')
+            plt.title(f'Initial Electron at {int(initial_eV/1000)}keV For {event_names[i]} Mean Occurance vs Simulations Ran')
+            plt.xlabel('Simulation Runs')
+            plt.ylabel('Mean Occurance')
             plt.axhline(y=plus_2, color='green', linestyle='--', label='+2%')
             plt.axhline(y=minus_2, color='red', linestyle='--', label='-2%')
-            plt.plot(x_values, mean_data[:, i]) 
+            plt.plot(x_values, batch_data[:, i]) 
             plt.legend()
-            
             pdf.savefig()
             plt.close()
 
-# n_sims = 1000
-# ev = 1000
-# batch_1kev, t_e = run_simulations(ev, n_sims)
-# arr = []
-# for batch in batch_1kev:
-#     batch = batch * delta_k
-#     arr.append(batch.sum() - batch[10])
 
-# print("Energy \t terminating energy")
-# for i in range(len(arr)):
-#     print(f"{arr[i]} + {t_e[i]} = {arr[i] + t_e[i]}")
+data = get_batch_data(20000, 100000)
+create_pdf(data, 20000, 100000)
 
-# n_sims = 100000
-# ev = 20000
+data = get_batch_data(60000, 100000)
+create_pdf(data, 60000, 100000)
 
-# batch_20kev = run_simulations(ev, n_sims)
-
-# n_sims = 100000
-# ev = 60000
-
-# batch_60kev = run_simulations(ev, n_sims)
-
-
-# np.save("batch20kev.npy", batch_20kev)
-
-# np.save('batch_60kev.npy', batch_60kev)
-
-# batch_20kev = np.load('batch2kev.npy')
-batch_1kev = np.load('batch1kev.npy')
-batch_100kev = np.load('batch100kev.npy')
-
-create_pdf(batch_100kev, 10000, 100000)
-create_pdf(batch_1kev, 1000000, 1000)
+data = get_batch_data(100000, 100000)
+create_pdf(data, 100000, 100000)
--- run_simulation.py
+++ run_simulation.py
@@ -22,7 +22,7 @@ def stack_pop(stack, top):
 def ion_event(E, index):
     u = np.random.rand()
     E = E - delta_k[index]
-    x_max = (E - delta_k[index]) / 2
+    x_max = (E) / 2
     E_new = (min_energy * x_max) / (x_max - u * (x_max - min_energy))
     E_old = E - E_new
     return E_old, E_new
@@ -75,100 +75,32 @@ def stack_gen_pop(E_stack, top):
     gen = E_stack[top, 1]
     return E, gen, top
 
-@njit
-def run_gen_sim(E):
-    E_stack = np.empty((20, 2), dtype=np.float64)
-
-    event_count = np.zeros(28, dtype=np.float64)
-    generation_event_count = np.zeros((100, 28), dtype=np.int64)
-
-    top = 0
-    top = stack_gen_push(E_stack, top, E, 0)
-
-    terminating_energy = 0
-
-    while top != 0:
-        E, gen, top = stack_gen_pop(E_stack, top)
-
-        indx = select_event(E)
-        event_count[indx] += 1
-        generation_event_count[gen, indx] += 1 
-
-        if indx < 7:
-            E_old, E_new = ion_event(E, indx)
-            next_gen = gen + 1
-
-            if E_old > min_energy:
-                top = stack_gen_push(E_stack, top, E_old, next_gen)
-            else:
-                terminating_energy += E_old
-
-            if E_new > min_energy:
-                top = stack_gen_push(E_stack, top, E_new, next_gen)
-            else:
-                terminating_energy += E_new
-
-        else:
-            if indx != 10:
-                E = E - delta_k[indx]
-                if E > min_energy:
-                    top = stack_gen_push(E_stack, top, E, gen)
-                else:
-                    terminating_energy += E
-            else:
-                terminating_energy += E
-
-    return event_count, generation_event_count
-
 
 @njit
-def run_simulations(E, N_sims):
-    next_free = 0
-    batch_size =  (N_sims+999)//1000
-    batch_simulations = np.zeros((batch_size, 28), dtype=np.float64)
-    t_e_range = np.zeros(batch_size, dtype=np.float64)
-    simulation_index = np.zeros(28, dtype=np.float64)
-    terminating_energy = 0
-    for r in range(N_sims):
-        index_list, t_e = run_sim(E)
-        terminating_energy += t_e
-        if (r+1) % 1000==0:
-            print(r+1, ' sims completed')
-        for i in range(28):
-            simulation_index[i] += index_list[i]
-        if (r + 1) % 1000 == 0 or (r + 1) == N_sims:
-            if next_free == 0:
-                batch_simulations[next_free] = simulation_index.copy()
-                t_e_range[next_free] = terminating_energy
-                
-            else:
-                batch_simulations[next_free] = batch_simulations[next_free-1] + simulation_index
-                t_e_range[next_free] = t_e_range[next_free-1] + terminating_energy
+def run_simulations(E, storage):
+    t_e_size = int(storage.shape[0])
+    t_e = np.empty(t_e_size, dtype=np.float64)
+    for i in prange(storage.shape[0]):
+        if (i+1) % 1000 == 0:
+            print(i+1, ' sims completed')
+        storage[i], t_e[i] = run_sim(E)
+    return storage, t_e
 
-            next_free+=1
-            simulation_index[:] = 0
-            terminating_energy = 0
-
+@njit
+def get_batch_data(eV, total_sims):
+    storage = np.empty((total_sims, 28), dtype=np.float64)
+    event_index, terminating_energy = run_simulations(eV, storage)
+    number_of_batches = int(total_sims / 1000)
+    x_values = np.arange(1000, 1001 * number_of_batches, 1000)
+    batch_storage = np.empty((number_of_batches, 28), dtype=np.float64)
+    for i in prange(number_of_batches):
+        start = i * 1000
+        end = (i+1) * 1000
+        batch_storage[i] = event_index[start:end].sum(axis=0)
+    for i in prange(batch_storage.shape[0]):
+        if i !=0:
+            batch_storage[i] += batch_storage[i-1]
+    for i in prange(batch_storage.shape[0]):
+        batch_storage[i] = batch_storage[i] / x_values[i]
         
-    return batch_simulations, t_e_range
-
-
-# event_index,terminating_energy = run_simulations(100000, 10000)
-# dict_event = dict(zip(events, event_index))
-
-# for k in dict_event:
-#      print(f'{k} = {dict_event[k]}')
-
-# energy_total = event_index * delta_k
-# print()
-# dict_energy = dict(zip(events, energy_total))
-
-# for k in dict_energy:
-#      print(f'{k} = {dict_energy[k]}')
-
-# print("Sum of All Events: ", energy_total.sum())
-# print(f'terminating energy: {terminating_energy}')
-# print("Total Energy: ", terminating_energy + energy_total.sum())
-
-# print(f"Total Number of Events: {event_index.sum()}") 
-
+    return batch_storage
